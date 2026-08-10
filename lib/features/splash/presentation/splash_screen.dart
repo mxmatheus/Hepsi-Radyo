037c01828da2fa567ad235f38e492558eaac6497
mxmatheus <batuhan.koksal699@gmail.com>
2026-08-11 02:13:50 +0300

    fix: splash ekranı logo yükleyici görsel yolları web ve mobil ortamları için dinamik hale getirildi

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -110,17 +110,16 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
                   height: 170,
                   fit: BoxFit.contain,
                   errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                        color: AppColors.racingGreenPrimary,
-                        borderRadius: BorderRadius.circular(32),
-                        border: Border.all(color: AppColors.wineRedAccent, width: 2),
-                        boxShadow: AppTokens.glowShadowGold,
-                      ),
-                      child: const Center(
-                        child: Icon(Icons.radio_rounded, size: 72, color: Colors.white),
+                    return Image.network(
+                      'app_logo.png',
+                      width: 170,
+                      height: 170,
+                      fit: BoxFit.contain,
+                      errorBuilder: (_, __, ___) => Image.network(
+                        'assets/images/app_logo.png',
+                        width: 170,
+                        height: 170,
+                        fit: BoxFit.contain,
                       ),
                     );
                   },
